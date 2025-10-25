6f849bf0e300f17b0627a80f3f9a465b5533762a
Victor <viktorstupin9@gmail.comgit config --global user.email viktorstupin9@mail.com>
2025-10-25 13:01:13 +0300

    build portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
-import React from 'react';
+import { Outlet } from 'react-router-dom';
 import './App.scss';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
-}
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
-
-export const App: React.FC = () => {
-  return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
+export const App = () => (
+  <div className="app-container">
+    <header>
+      <Navbar />
+    </header>
+    <div className="main-content">
+      <Outlet />
     </div>
-  );
-};
+    <Footer />
+  </div>
+);
